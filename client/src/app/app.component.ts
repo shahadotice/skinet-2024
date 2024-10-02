dd70bb2b536453fc45764e184ddb02b0dd681e72
Md. Shahadot Hossain <Shahadot.Hossain@brainstation-23.com>
2024-10-02 18:47:39 +0600

    End of  Section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/Product';
+import { Pagination } from './shared/models/Pagination';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'Skinet';
+  baseUrl ='https://localhost:5001/api/'
+  products:Product[]=[];
+  private http=inject(HttpClient);
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>(this.baseUrl+'products').subscribe({
+      next:response=>this.products=response.data,
+      error:error=>console.log(error),
+      complete:()=>console.log('complete')
+    })
+  }
 }
